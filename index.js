5ff0f775b466ec638c3df0045df1bb149f44df77
Kornel Lesiński <kornel@geekhood.net>
2016-05-21 14:20:47 +0100

    Hello

---
+++ index.js
@@ -0,0 +1,87 @@
+'use strict';
+
+const LRU = require('lru-cache');
+
+function Cache(options) {
+    if (!options) options = {};
+
+    this._errorTimeout = options.errorTimeout || 200;
+
+    this._storage = new LRU({
+        max: options.size || 500*1024*1024, // 500MB
+        length(obj) {
+            return obj.cost;
+        },
+        stale:false, // errors must expire
+        maxAge: options.maxAge || 24*3600*1000,
+    });
+}
+
+Cache.prototype = {
+    getCached(url, request, callback) {
+        if (!url || !request || !callback) throw Error("Bad cache args");
+
+        const cached = this._storage.get(url);
+        if (cached) {
+            return cached.promise;
+        }
+
+        const resultPromise = Promise.resolve({}).then(callback).then(res => {
+            const maxAge = this._maxAgeForResponse(res);
+            if (maxAge) {
+                const cost = 4000 + (Buffer.isBuffer(res.body) ? res.body.byteLength : 8000);
+                this._storage.set(url, {cost, promise:resultPromise}, maxAge);
+            }
+            return res;
+        }, err => {
+            this._storage.set(url, {cost: 30000, promise:resultPromise}, this._errorTimeout);
+            throw err;
+        });
+        return resultPromise;
+    },
+
+    _maxAgeForResponse(res) {
+        if (!res || !res.headers || !res.body) {
+            console.warn("Suspicious response", res);
+            return 0;
+        }
+
+        if (res.headers['vary']) {
+            const tokens = res.headers['vary'].split(/[\s,]+/).filter(t => !/^(?:accept-charset|accept-encoding|host|accept|origin)$/i.test(t));
+            if (tokens.length) {
+                return 0; // TODO
+            }
+        }
+
+        if (res.headers['expires']) {
+            const expires = Date.parse(res.headers['expires']);
+            if (isFinite(expires)) {
+                let serverTime = Date.parse(res.headers['date'])
+                const now = Date.now();
+                const maxClockDrift = 8*3600*1000;
+                if (!isFinite(serverTime) || serverTime < now-maxClockDrift || serverTime > now+maxClockDrift) {
+                    serverTime = now;
+                }
+                if (expires < serverTime) {
+                    return 0;
+                }
+            }
+        }
+
+        const cc = res.headers['cache-control'];
+        if (cc) {
+            if (/private|no-cache|no-store|must-revalidate|proxy-revalidate/.test(cc)) return 0;
+            const m = cc.match(/s-maxage=([0-9]+)/);
+            if (m) {
+                return m[1]*1000;
+            }
+            const n = cc.match(/max-age=([0-9]+)/);
+            if (n) {
+                return n[1]*1000;
+            }
+        }
+        return 0;
+    },
+}
+
+module.exports = Cache;
---
+++ test/cachetest.js
@@ -0,0 +1,178 @@
+'use strict';
+
+const assert = require('assert');
+const Cache = require('..');
+
+function mockResponseWith(headers) {
+    return {
+        testedObject:true,
+        body: new Buffer(0),
+        headers,
+    };
+}
+
+describe('Cache', function() {
+    it('simple miss', function() {
+        const cache = new Cache();
+        let called = false;
+        return cache.getCached('http://foo.bar/baz.quz', {}, reqOpts => {
+            assert(reqOpts);
+            called = true;
+            return mockResponseWith({});
+        }).then(res => {
+            assert(called);
+            assert(res);
+            assert(res.testedObject);
+        });
+    });
+
+    it('simple hit', function() {
+        const cache = new Cache();
+        let missInParallelCalled = false;
+        return cache.getCached('http://foo.bar/baz.quz', {}, reqOpts => {
+            assert(reqOpts);
+            return mockResponseWith({
+                'cache-control': 'public, max-age=999999',
+                'orig': 'yes',
+            })
+        }).then(res => {
+            assert(res.testedObject);
+            return Promise.all([
+                cache.getCached('http://foo.bar/baz.quz', {}, opt => assert.fail("should cache", opt)),
+                cache.getCached('https://foo.bar/baz.quz', {}, reqOpts => {
+                    assert(reqOpts);
+                    missInParallelCalled = true;
+                    return mockResponseWith({});
+                }),
+            ]);
+        }).then(tmp => {
+            assert(tmp[0].testedObject);
+            assert(tmp[1].testedObject);
+            assert.equal(tmp[0].headers.orig, 'yes');
+            assert(missInParallelCalled);
+        });
+    });
+
+    it('cache error', function() {
+        const cache = new Cache();
+        return cache.getCached('http://foo.bar/baz.quz', {}, () => {
+            throw Error("test");
+        }).then(res => assert.fail("nope", res), err => {
+            assert.equal("test", err.message);
+            return cache.getCached('http://foo.bar/baz.quz', {}, () => {
+                assert.fail("don't call");
+            });
+        }).then(res => assert.fail("nope", res), err => {
+            assert.equal("test", err.message);
+        });
+    });
+
+    it('cache error expires', function() {
+        const cache = new Cache({errorTimeout: 5});
+        return cache.getCached('http://foo.bar/baz.quz', {}, () => {
+            throw Error("test");
+        }).then(res => assert.fail("nope", res), err => {
+            assert.equal("test", err.message);
+            return new Promise(res => setTimeout(res, 6));
+        }).then(() => {
+            return cache.getCached('http://foo.bar/baz.quz', {}, () => {
+                return mockResponseWith({'expir':'ed'});
+            });
+        }).then(res => {
+            assert.equal(res.headers.expir, 'ed');
+        });
+    });
+
+    it('cache error per url', function() {
+        const cache = new Cache();
+        return cache.getCached('http://foo.bar/baz.quz', {}, () => {
+            throw Error("test");
+        }).then(res => assert.fail("nope", res), err => {
+            assert.equal("test", err.message);
+        }).then(() => {
+            return cache.getCached('http://foo.bar/baz.quz?', {}, () => {
+                return mockResponseWith({'got':'it'});
+            });
+        }).then(res => {
+            assert(res.headers);
+            assert.equal(res.headers.got, 'it');
+        });
+    });
+
+    it('miss private cache', function() {
+        const cache = new Cache();
+        let called = 0;
+        return cache.getCached('http://foo.bar/baz.quz', {}, reqOpts => {
+            assert(reqOpts);
+            called++;
+            return mockResponseWith({
+                'cache-control': 'private, max-age=999999',
+            });
+        }).then(res => {
+            assert(res.testedObject);
+            return cache.getCached('http://foo.bar/baz.quz', {}, () => {
+                called++;
+                return mockResponseWith({
+                    'cache-control': 'private, max-age=333',
+                    'second': 'yes',
+                });
+            });
+        }).then(res => {
+            assert(res.testedObject);
+            assert.equal(2, called);
+            assert.equal(res.headers.second, 'yes');
+        });
+    });
+
+    it('miss max-age=0', function() {
+        const cache = new Cache();
+        let called = 0;
+        return cache.getCached('http://foo.bar/baz.quz', {}, reqOpts => {
+            assert(reqOpts);
+            called++;
+            return mockResponseWith({
+                'cache-control': 'public, max-age=0',
+            });
+        }).then(res => {
+            assert(res.testedObject);
+            return cache.getCached('http://foo.bar/baz.quz', {}, () => {
+                called++;
+                return mockResponseWith({
+                    'cache-control': 'public, max-age=333',
+                    'second': 'yes',
+                });
+            });
+        }).then(res => {
+            assert(res.testedObject);
+            assert.equal(2, called);
+            assert.equal(res.headers.second, 'yes');
+        });
+    });
+
+    it('miss expires', function() {
+        const cache = new Cache();
+        let called = 0;
+        return cache.getCached('http://foo.bar/baz.quz', {}, reqOpts => {
+            assert(reqOpts);
+            called++;
+            return mockResponseWith({
+                'cache-control': 'public, max-age=9999',
+                'expires': 'Sat, 07 May 2016 15:35:18 GMT',
+            });
+        }).then(res => {
+            assert(res.testedObject);
+            return cache.getCached('http://foo.bar/baz.quz', {}, () => {
+                called++;
+                return mockResponseWith({
+                    'cache-control': 'public, max-age=333',
+                    'expires': new Date(Date.now()+3600*1000).toGMTString(),
+                    'second': 'yes',
+                });
+            });
+        }).then(res => {
+            assert(res.testedObject);
+            assert.equal(2, called);
+            assert.equal(res.headers.second, 'yes');
+        });
+    });
+});
